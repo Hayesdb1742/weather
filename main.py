a5a7b56bbefdd9ba3825b136eea1deec73aeda6d
Hayesdb1742 <hayesdbentley1@live.com>
2024-01-25 09:01:00 -0500

    added notebook

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ api_token = "btjZHTwIomIDkazihXanMyPwZaDKDzJX"
 
 
 api_endpoint = "https://www.ncei.noaa.gov/cdo-web/api/v2/"
-url = api_endpoint + "data?datasetid=GHCND"
+url = api_endpoint + "data"
 headers = {'token': api_token}
-params = {'datasetid': "GHCND"}
-r = requests.get(url = url, headers=headers)
+PARAMS = {'datasetid': "GHCND", 'startdate': "2020-01-01", 'enddate': "2020-06-01", 'locationid': 'ZIP:43338'}
+r = requests.get(url = url, headers=headers, params=PARAMS)
 if r.status_code == 200:
     json = r.json()
     print(json)
 else:
     print("not a valid endpoint")
-    print(r)
+    print(r.content)
